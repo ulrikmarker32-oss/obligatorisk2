240c473090ed46a514248d53d14ef4b1ce360870
Ulrik Marker <ulrikmarker32@gmail.com>
2025-10-13 17:02:01 +0200

    Update and rename vis alle studenter to visallestudenter.php

---
+++ visallestudenter.php
@@ -0,0 +1,25 @@
+<?php /* vis-alle-emner */
+/*
+/* Programmet skriver ut alle registrerte emner
+*/
+include("db-tilkobling.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
+$sqlSetning="SELECT * FROM student ORDER BY brukernavn;";
+$sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen"); /*
+SQL-setning sendt til database-serveren */
+$antallRader=mysqli_num_rows($sqlResultat); /* antall rader i resultatet beregnet */
+print ("<h3>Registrerte studenter </h3>");
+print ("<table border=1>");
+print ("<tr><th align=left>brukernavn</th> <th align=left>fornavn</th> <th allign=left>etternavn</th> <th align=left>klassekode</th>
+</tr>");
+for ($r=1;$r<=$antallRader;$r++)
+{
+$rad=mysqli_fetch_array($sqlResultat); /* ny rad hentet fra spørringsresultatet */
+$brukernavn=$rad["brukernavn"];
+$fornavn=$rad["fornavn"];
+$etternavn=$rad["etternavn"];
+$klassekode=$rad["klassekode]
+print ("<tr> <td> $brukernavn </td> <td> $fornavn </td> <td> $etternavn </td> <td> $klassekode </td> </tr>");
+}
+print ("</table>");
+?>
+
