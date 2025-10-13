39be2c4f290daffe7793dd98919ee59222e2facc
Ulrik Marker <ulrikmarker32@gmail.com>
2025-10-13 16:21:38 +0200

    Update and rename slett klasse to slettklasse.php

---
+++ slettklasse.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<script src="funksjoner.js"> </script>
+<h3>Slett klasse <h3>
+<form method="post" action="" id="slettKlasseSkjema" name="slettKlasseSkjema" onSubmit="return bekreft()">
+Klassekode <input type="text" id="klassekode" name="klassekode" required /> <br/>
+<input type="submit" value="Slett klasse" name="slettKlasseKnapp" id="slettKlasseKnapp" />
+</form>
+
+<?php
+if (isset($_POST ["slettKlasseKnapp"]))
+{
+include("db-tilkobling.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
+$klassekode=$_POST ["klassekode"];
+$sqlSetning="DELETE FROM studium WHERE klassekode='$klassekode';";
+mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
+/* SQL-setning sendt til database-serveren */
+print ("F&oslash;lgende studium er n&aring; slettet: $klassekode <br />");
+}
+?>
