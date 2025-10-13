f0cff8bc73dbf7808fce5b1e8b1fc05740f5c257
Ulrik Marker <ulrikmarker32@gmail.com>
2025-10-13 16:53:32 +0200

    Update and rename slett student to slettstudent.php

---
+++ slettstudent.php
@@ -0,0 +1,21 @@
+<?php
+?>
+<h3>Slett student <h3>
+<form method="post" action="" id="slettStudentSkjema" name="slettStudentSkjema">
+Brukernavn <input type="text" id="brukernavn" name="brukernavn" required /> <br/>
+<input type="submit" value="Slett student" name="slettStudentKnapp" id="slettStudentKnapp" />
+</form>
+
+<?php
+if (isset($_POST ["slettStudentKnapp"]))
+{
+include("db-tilkobling.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
+$brukernavn=$_POST ["brukernavn"];
+$fornavn=$_POST ["fornavn"];
+$etternavn=$_POST ["etternavn"];
+$sqlSetning="DELETE FROM klasse WHERE brukernavn='$brukernavn';";
+mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
+/* SQL-setning sendt til database-serveren */
+print ("F&oslash;lgende student er n&aring; slettet: $fornavn $etternavn <br />");
+}
+?>
