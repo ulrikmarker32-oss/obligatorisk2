afbad6f97e9483d0b298a0059a6784d0357c6968
Ulrik Marker <ulrikmarker32@gmail.com>
2025-10-13 16:56:47 +0200

    Update slettstudent.php

--- slettstudent.php
+++ slettstudent.php
@@ -11,7 +11,7 @@ if (isset($_POST ["slettStudentKnapp"]))
 {
 include("db-tilkobling.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
 $brukernavn=$_POST ["brukernavn"];
-$sqlSetning="DELETE FROM klasse WHERE brukernavn='$brukernavn';";
+$sqlSetning="DELETE FROM student WHERE brukernavn='$brukernavn';";
 mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
 /* SQL-setning sendt til database-serveren */
 print ("F&oslash;lgende student er n&aring; slettet: $brukernavn <br />");
