0bdcb332e8fcca44f1248e90825f8195578d6351
Ulrik Marker <ulrikmarker32@gmail.com>
2025-10-13 15:21:15 +0200

    Create db-tilkobling.php

---
+++ db-tilkobling.php
@@ -0,0 +1,9 @@
+<?php 
+
+$host = ulmar4697('DB_HOST');
+$username = ulmar4697('DB_USER');
+$password = eaddulmar4697('DB_PASSWORD');
+$database = ulmar4697('DB_DATABASE');
+$db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
+
+?>
