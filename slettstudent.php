6da25be10f769255d096aaafc0ee927d163bb5af
Ulrik Marker <ulrikmarker32@gmail.com>
2025-10-22 20:37:08 +0200

    Update slettstudent.php

--- slettstudent.php
+++ slettstudent.php
@@ -5,7 +5,20 @@
 
 <h3>Slett student <h3>
 <form method="post" action="" id="slettStudentSkjema" name="slettStudentSkjema" onsubmit="return bekreft()">
-Brukernavn <input type="text" id="brukernavn" name="brukernavn" required /> <br/>
+Brukernavn:
+  <select id="brukernavn" name="brukernavn" required>
+    <option value="">-- Velg student --</option>
+    <?php
+include("db-tilkobling.php");
+$sqlSetning = "SELECT brukernavn FROM student ORDER BY brukernavn;";
+$sqlResultat = mysqli_query($db, $sqlSetning) or die ("Ikke mulig &aring; hente brukernavn");
+while ($rad = mysqli_fetch_array($sqlResultat)) {
+  $brukernavn = $rad["brukernavn"]
+    echo "<option value='$brukernavn'>$brukernavn</option>";
+}
+?>
+  </select>
+  <br><br>
 <input type="submit" value="Slett student" name="slettStudentKnapp" id="slettStudentKnapp" />
 </form>
 
