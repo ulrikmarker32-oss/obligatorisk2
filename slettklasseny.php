1c5e0f95f6db5579722ff2fcfd55066e11e0d216
Ulrik Marker <ulrikmarker32@gmail.com>
2025-10-23 10:18:06 +0200

    Refactor klasse deletion form and error handling

--- slettklasseny.php
+++ slettklasseny.php
@@ -1,32 +1,33 @@
 <?php
+include("db-tilkobling.php");
 ?>
 
-<script src="funksjoner.js"> </script>
+<script src="funksjoner.js"></script>
 
-<h3>Slett klasse <h3>
+<h3>Slett klasse</h3>
+
 <form method="post" action="" id="slettKlasseSkjema" name="slettKlasseSkjema" onsubmit="return bekreft()">
-Klassekode:
+  Klassekode:
   <select id="klassekode" name="klassekode" required>
     <option value="">-- Velg klasse --</option>
     <?php
-include("db-tilkobling.php");
-$sqlSetning = "SELECT klassekode FROM klasse ORDER BY klassekode;";
-$sqlResultat = mysqli_query($db, $sqlSetning) or die ("ikke mulig &aring; hente klassekoder");
-while ($rad = mysqli_fetch_array($sqlResultat)) {
-  $klassekode = $rad["klassekode"];
-  echo "<option value='$klassekode'>$klassekode</option>";
-}
-?>
+    $sqlSetning = "SELECT klassekode FROM klasse ORDER BY klassekode;";
+    $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å hente klassekoder");
+    
+    while ($rad = mysqli_fetch_array($sqlResultat)) {
+      $klassekode = htmlspecialchars($rad["klassekode"]);
+      echo "<option value='$klassekode'>$klassekode</option>";
+    }
+    ?>
   </select>
   <br><br>
-  
-<input type="submit" value="Slett klasse" name="slettKlasseKnapp" id="slettKlasseKnapp" />
+  <input type="submit" value="Slett klasse" name="slettKlasseKnapp" id="slettKlasseKnapp" />
 </form>
 
 <?php
 if (isset($_POST["slettKlasseKnapp"])) {
   include("db-tilkobling.php"); 
- $klassekode=$_POST ["klassekode"];
+  $klassekode = mysqli_real_escape_string($db, $_POST["klassekode"]);
 
   $sqlSetning = "DELETE FROM klasse WHERE klassekode='$klassekode';";
   $resultat = mysqli_query($db, $sqlSetning);
@@ -36,9 +37,14 @@ if (isset($_POST["slettKlasseKnapp"])) {
   } else {
     $feilmelding = mysqli_error($db);
     
-   
+    // Sjekk om feilen skyldes foreign key constraint (studenter tilknyttet)
     if (str_contains($feilmelding, 'foreign key constraint')) {
-      print("Kan ikke slette klassen <strong>$klassekode</strong> fordi det finnes studenter registrert i denne klassen.<br>");
+      print("<span style='color:red;'>Kan ikke slette klassen <strong>$klassekode</strong> fordi det finnes studenter registrert i denne klassen.</span><br>");
     } else {
-      print("En feil oppstod ved sletting av klasse. Vennligst prøv igjen eller kontakt administrator.</span><br>");
-     
+      print("<span style='color:red;'>En feil oppstod ved sletting av klasse. Vennligst prøv igjen eller kontakt administrator.</span><br>");
+      // (valgfritt) logg feilen for debugging:
+      // error_log($feilmelding);
+    }
+  }
+}
+?>
